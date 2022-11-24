9efae9554161e59ab01d7227b17158b7e46ff52d
naa15 <naltu15@freeuni.edu.ge>
2022-11-24 22:03:48 +0400

    add PaymentRequestPayload class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,86 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        String filename = "Payment Request Payload.csv";
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            myReader.useDelimiter(",");
+            while (myReader.hasNext()) {
+//                String data = myReader.next();
+                PaymentRequestPayload p = new PaymentRequestPayload();
+                String lvl = myReader.next();
+                p.setLvl(Integer.valueOf(lvl));
+                p.setName(myReader.next());
+                p.setTag(myReader.next());
+                p.setOccur(myReader.next());
+                String type = myReader.next();
+                if(p.getTag().equals("")) {
+                    p.setCode(type);
+                } else {
+                    while(true) {
+                        String s = myReader.next();
+                        if(s.length() != 0 && !s.startsWith("-")) {
+                            type = type + s;
+                        } else {
+                            p.setType(type);
+                            p.setValidationRule(s);
+                            break;
+                        }
+                    }
+                }
+                p.setPath(myReader.next());
+                String definition = myReader.next();
+                while(true) {
+                    String s = myReader.next();
+                    if(s.length() != 0) {
+                        definition = definition + s;
+                    } else {
+                        p.setDefinition();
+                        break;
+                    }
+                }
+                p.setFieldDefinition(myReader.next());
+
+                System.out.println(p.toString());
+                myReader.next();
+//                System.out.println(data);
+//                System.out.println("new one:");
+//                allData.add(data);
+            }
+//            convert();
+
+            // /Request/RequestPayload/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf/RmtInf/Strd/AddtlRmtInf
+            //
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
     }
+
+//    private static void xmlBeansTest(String xsdFile) throws XmlException, IOException {
+//        SchemaTypeSystem sts = XmlBeans.compileXsd(
+//                new XmlObject[] { XmlObject.Factory.parse(new File(xsdFile)) }, XmlBeans.getBuiltinTypeSystem(), null);
+//
+//        for (int i = 0; i < sts.globalElements().length; i++) {
+//            SchemaGlobalElement globalElement = sts.globalElements()[i];
+//            SchemaType type = globalElement.getType();
+//            System.out.println(globalElement.getName() + " = " + type.getName());
+//
+//            for (int j = 0; j < type.getProperties().length; j++) {
+//                SchemaProperty property = type.getProperties()[j];
+//                System.out.println("\t" + property.getName() +
+//                        " [" + property.getMinOccurs() + ", " + property.getMaxOccurs() + "]" +
+//                        " = " + property.getType().getName());
+//            }
+//        }
+//    }
 }
---
+++ src/main/java/org/example/PaymentRequestPayload.java
@@ -0,0 +1,117 @@
+package org.example;
+
+public class PaymentRequestPayload {
+    private int lvl;
+    private String name;
+    private String tag="";
+    private String occur;
+
+    @Override
+    public String toString() {
+        return "PaymentRequestPayload{" +
+                "lvl=" + lvl +
+                ", name='" + name + '\'' +
+                ", tag='" + tag + '\'' +
+                ", occur='" + occur + '\'' +
+                ", type='" + type + '\'' +
+                ", code='" + code + '\'' +
+                ", validationRule='" + validationRule + '\'' +
+                ", path='" + path + '\'' +
+                ", definition='" + definition + '\'' +
+                ", fieldDefinition='" + fieldDefinition + '\'' +
+                '}';
+    }
+
+    private String type="";
+    private String code="";
+    private String validationRule="";
+    private String path;
+    private String definition;
+    private String fieldDefinition;
+
+    public PaymentRequestPayload() {
+
+    }
+
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setOccur(String occur) {
+        this.occur = occur;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setValidationRule(String validationRule) {
+        this.validationRule = validationRule;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public void setFieldDefinition(String fieldDefinition) {
+        this.fieldDefinition = fieldDefinition;
+    }
+
+    public int getLvl() {
+        return lvl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getOccur() {
+        return occur;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getValidationRule() {
+        return validationRule;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public String getFieldDefinition() {
+        return fieldDefinition;
+    }
+
+}
